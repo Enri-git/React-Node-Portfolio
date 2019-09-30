2a95ce669ad6a3a5e681d9db0610b25a2dd4b12b
Enri-git <berti.enrica@gmail.com>
2019-09-30 16:45:21 +0200

    u3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path = require('path');
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('./', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname + '/client/public/index.html'));
     })
 }
